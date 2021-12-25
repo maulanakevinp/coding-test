4953205bd4367f3d4647afcc4da3eb7634fead62
Maulana Kevin Pradana <maulanakevinp@gmail.com>
2021-12-26 04:18:04 +0700

    update alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="{{ asset('@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/sweetalert.min.js') }}"></script>
     @yield('styles')
 </head>
 <body style="background-color:#1c1e21">
